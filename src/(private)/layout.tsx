02a2219e2c20ac7ab3089b3125d5cb493cfb1a84
ricardoabicalhob <rbicalho2014@gmail.com>
2025-06-04 22:24:49 -0300

    adicionar correção no baseurl em layout.tsx

--- src/(private)/layout.tsx
+++ src/(private)/layout.tsx
@@ -81,41 +81,43 @@ function SubMenuBar({ perfil } :SubMenuBarProps) {
       dateRange, setDateRange
   } = useContext(SystemContext)
 
+  const basename = import.meta.env.BASE_URL
+
   return(
     <div className="flex items-center px-2 border-b border-b-gray-200 divide-x divide-gray-300 h-12 w-full bg-white">
       <DateRangePicker dateRange={dateRange} setDateRange={setDateRange}/>
 
       { perfil.gerador &&
-        (pathname === "/gerador" ?
+        (pathname === `${basename}/gerador` ?
         <span className="text-[#00695C] font-normal leading-relaxed px-2 select-none">Gerador</span> :
-        <a href="/gerador" className="font-light px-2">Gerador</a>) }
+        <a href={`${basename}/gerador`} className="font-light px-2">Gerador</a>) }
       
       { perfil.armazenadorTemporario &&
-        (pathname === "/armazenador-temporario" ?
+        (pathname === `${basename}/armazenador-temporario` ?
         <span className="text-[#00695C] font-normal leading-relaxed px-2 select-none">Armazenador Temporário</span> :
-        <a href="/armazenador-temporario" className="font-light px-2">Armazenador Temporário</a>) }
+        <a href={`${basename}/armazenador-temporario`} className="font-light px-2">Armazenador Temporário</a>) }
       
       { perfil.destinador &&
-        (pathname === "/destinador" ?
+        (pathname === `${basename}/destinador` ?
         <span className="text-[#00695C] font-normal leading-relaxed select-none px-2">Destinador</span> :
-        <a href="/destinador" className="font-light px-2">Destinador</a>) }
+        <a href={`${basename}/destinador`} className="font-light px-2">Destinador</a>) }
 
       { perfil.gerador && perfil.armazenadorTemporario &&
-        (pathname === "/movimentacao-para-o-destinador-final" ?
+        (pathname === `${basename}/movimentacao-para-o-destinador-final` ?
         <span className="text-[#00BCD4] font-normal leading-relaxed select-none pl-2">Movimentação para o destinador final</span> :
-        <a href="/movimentacao-para-o-destinador-final" className="font-light pl-2">Movimentação para o destinador final</a>  
+        <a href={`${basename}/movimentacao-para-o-destinador-final`} className="font-light pl-2">Movimentação para o destinador final</a>  
         ) }
 
       { !perfil.destinador && !perfil.armazenadorTemporario &&
-        (pathname === "/movimentacao-gerador-para-o-armazenador-temporario" ?
+        (pathname === `${basename}/movimentacao-gerador-para-o-armazenador-temporario` ?
         <span className="text-[#00BCD4] font-normal leading-relaxed select-none px-2">Minhas movimentações para o armazenamento temporário</span> :
-        <a href="/movimentacao-gerador-para-o-armazenador-temporario" className="font-light px-2">Minhas movimentações para o armazenamento temporario</a>  
+        <a href={`${basename}/movimentacao-gerador-para-o-armazenador-temporario`} className="font-light px-2">Minhas movimentações para o armazenamento temporario</a>  
         ) }
 
       { !perfil.destinador && !perfil.armazenadorTemporario &&
-        (pathname === "/movimentacao-gerador-para-o-destinador-final" ?
+        (pathname === `${basename}/movimentacao-gerador-para-o-destinador-final` ?
         <span className="text-[#00BCD4] font-normal leading-relaxed select-none pl-2">Minhas movimentações para o destinador final</span> :
-        <a href="/movimentacao-gerador-para-o-destinador-final" className="font-light pl-2">Minhas movimentações para o destinador final</a>  
+        <a href={`${basename}/movimentacao-gerador-para-o-destinador-final`} className="font-light pl-2">Minhas movimentações para o destinador final</a>  
         ) }      
     </div>
   )
