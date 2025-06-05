e662d60cecd3338bdd086813b3b88b1182d93093
ricardoabicalhob <rbicalho2014@gmail.com>
2025-06-04 22:15:36 -0300

    modificar a abordagem de roteamento incluindo o componente RootRedirector

--- src/main.tsx
+++ src/main.tsx
@@ -11,13 +11,13 @@ import {
   Navigate,
 } from "react-router-dom"
 
-import React from 'react'
+import React, { useContext } from 'react'
 import { queryClient } from './services/queryClient.ts'
 import { Toaster } from 'sonner'
 
 import SignIn from './(public)/sign-in/page.tsx'
 import PrivateLayout from './(private)/layout.tsx'
-import { AuthProvider } from './contexts/auth.context.tsx'
+import { AuthContext, AuthProvider } from './contexts/auth.context.tsx'
 import GeradorPage from './(private)/gerador/page.tsx'
 import DestinadorPage from './(private)/destinador/page.tsx'
 import VisaoGeralPage from './(private)/movimentacao-para-o-destinador-final/page.tsx'
@@ -25,18 +25,41 @@ import ArmazenadorTemporarioPage from './(private)/armazenador-temporario/page.t
 import MovimentacaoParaATPage from './(private)/movimentacao-gerador-para-o-armazenador-temporario/page.tsx'
 import MovimentacaoParaDFPage from './(private)/movimentacao-gerador-para-o-destinador-final/page.tsx'
 
-// const basename = import.meta.env.BASE_URL
+const basename = import.meta.env.BASE_URL
+
+const RootRedirector = ()=> {
+  const { isLoadingAuth, loginResponse } = useContext(AuthContext)
+
+  if(isLoadingAuth) {
+    return <div>Carregando autenticação...</div>
+  }
+
+  if(!!loginResponse) {
+    return <Navigate to="/gerador" replace />
+  }
+
+  return <Navigate to="/sign-in" replace />
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-      <BrowserRouter basename={'/dashboard-sinir-react/'}>
+      <BrowserRouter basename={basename}>
         <AuthProvider>
           <QueryClientProvider client={queryClient}>
             <Routes>
-              <Route path='/' element={<Navigate to={'/sign-in'} replace />} />
+              {/* <Route path='/' element={<Navigate to={'/sign-in'} replace />} /> */}
               <Route path='/sign-in' element={<SignIn />} />
               
-              <Route path='/' element={<PrivateLayout />} >
+              <Route path='/' element={<RootRedirector />} />
+              <Route element={<PrivateLayout />}>
+                <Route path='/gerador' element={<GeradorPage />} />
+                <Route path='/destinador' element={<DestinadorPage />} />
+                <Route path='/movimentacao-para-o-destinador-final' element={<VisaoGeralPage />} />
+                <Route path='/armazenador-temporario' element={<ArmazenadorTemporarioPage />} />
+                <Route path='/movimentacao-gerador-para-o-armazenador-temporario' element={<MovimentacaoParaATPage />} />
+                <Route path='/movimentacao-gerador-para-o-destinador-final' element={<MovimentacaoParaDFPage />} />
+              </Route>
+              {/* <Route path='/' element={<PrivateLayout />} >
                 <Route index element={<Navigate to={'/gerador'} replace />} />
                 
                 <Route path='/gerador' element={<GeradorPage />} />
@@ -45,7 +68,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
                 <Route path='/armazenador-temporario' element={<ArmazenadorTemporarioPage />} />
                 <Route path='/movimentacao-gerador-para-o-armazenador-temporario' element={<MovimentacaoParaATPage />} />
                 <Route path='/movimentacao-gerador-para-o-destinador-final' element={<MovimentacaoParaDFPage />} />
-              </Route>
+              </Route> */}
             </Routes>
           </QueryClientProvider> 
           <Toaster />
